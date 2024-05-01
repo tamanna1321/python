de570a715445ac4f3c6ac51064380c73cc02b11b
tamanna1321 <159517381+tamanna1321@users.noreply.github.com>
2024-05-01 17:27:07 +0530

    operators.py

---
+++ opera.py
@@ -0,0 +1,66 @@
+print("Operators")
+#arithmetic operators
+a=int(input("Enter a "))
+b= int(input("Enter b "))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+
+
+#logical operators
+print(True and True)
+print(True or True)
+print(False and True)
+print(True and False)
+print(True or False)
+print(not True)
+
+
+#assignment operators
+a=10
+a+=10
+a*=5
+a/=10
+a-=5
+print(a)
+
+
+
+
+#relational operators
+a=20
+b=20
+print(a==b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+
+#membership operators
+a="Bhumika"
+print("umik" not in a)
+print("umik" in a)
+
+#identity operators
+b="Tamanna"
+
+print(a is b)
+print(a is not b)
+
+a=14
+
+# if statement (else if is elif in python)
+if a>10:
+    print("Greater than 10")
+elif a==10:
+    print("Equal to 10")
+else:
+    print("Less than 10")
+
+
+
+
+
