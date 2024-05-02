c988fbebb1ec8f645995553003a842083df2db03
tamanna1321 <159517381+tamanna1321@users.noreply.github.com>
2024-05-02 17:09:48 +0530

    python dictionary

---
+++ diction.py
@@ -0,0 +1,13 @@
+#WAP TO CREATE FOLLOWING STRUCTURE
+# SAMPLE LISTS["BLACK","RED","MAROON","YELLOW"]
+colour_name=["Black","Red","Maroon","Yellow"]
+colour_code=["#000000","FF0000","#800000","#FFFF00"]
+result=[{"colour_name":colour_name[i],"colour_code":colour_code[i]}
+        for i in range(len(colour_name))]
+print(result)
+
+
+#WAP TO FIND THE SUBJECT WITH SECOND HIGHEST MARKS IN {'ENG':100,'HINDI':20,'SOCIAL':45,'PUNJABI':85}
+marks={'eng':100,'hindi':20,'social':45,'punjabi':85}
+print("highest marks:",sorted(marks.values())[-1])
+print("second highest marks:",sorted(marks.values())[-2])
